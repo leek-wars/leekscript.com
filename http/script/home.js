005e445e07b14d062a15ea22c64ff227d60e96cb
Romain <Becold@users.noreply.github.com>
2016-08-29 07:08:06 +0200

    Useless console.log removed
    - lol

--- http/script/home.js
+++ http/script/home.js
@@ -5,7 +5,6 @@ leekscript.controller('home_controller', function($scope, $route, $http) {
 	$scope.console = "let n = [1, 2, 3]\nlet carrés = n ~~ x -> x ^ 2\ncarrés.sum()\n\n// or\n\n[1, 2, 3].map(x -> x ^ 2).sum()";
 
 	$scope.keypressed = function(event) {
-		console.log("lol", event)
 		if (event.keyCode == 13) {
 			$scope.execute();
 			event.stopPropagation();
