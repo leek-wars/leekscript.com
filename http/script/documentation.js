9a3aae0f410af5762858c1df2a0f0c9a5772c430
Pilow <pierre.laupretre@gmail.com>
2017-07-07 01:26:03 +0200

    [doc] Move documentation file into http/

--- http/script/documentation.js
+++ http/script/documentation.js
@@ -2,7 +2,7 @@ leekscript.controller('documentation_controller', function($scope, $route, $http
 
 	$scope.documentation = {};
 
-	$http.get('/api/documentation/get').success(function(data) {
-		$scope.documentation = data.documentation;
+	$http.get('documentation.json').success(function(data) {
+		$scope.documentation = data;
 	})
 })
