b47681e833453b477b2f24de1285f4e1db5d27c7
Pilow <pierre.laupretre@gmail.com>
2017-07-07 02:20:42 +0200

    [home] Fix leekscript services

--- http/script/home.js
+++ http/script/home.js
@@ -1,5 +1,7 @@
 leekscript.controller('home_controller', function($scope, $route, $http) {
 
+	$http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+
 	$scope.Math = window.Math;
 
 	$scope.console = "let n = [1, 2, 3]\nlet carrés = n ~~ x -> x ^ 2\ncarrés.sum()\n\n// or\n\n[1, 2, 3].map(x -> x ^ 2).sum()";
@@ -13,16 +15,29 @@ leekscript.controller('home_controller', function($scope, $route, $http) {
 		}
 	}
 
+
+
 	$scope.execute = function() {
-		$http.post('/api/code/execute', {code: $scope.console}).success(function(data) {
+		$http({
+	        url: 'https://leekwars.com/api/leekscript/execute',
+	        method: 'POST',
+	        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+	        transformRequest: function(obj) {
+	            var str = [];
+	            for (var p in obj) str.push(encodeURIComponent(p) + ' = ' + encodeURIComponent(obj[p]));
+	            return str.join('&');
+	        },
+			cache: false,
+	        data: {code: $scope.console, token: 'a'},
+	    }).success(function(data) {
 			var result = JSON.parse(data.result)
 			console.log(result)
 			$scope.result = result;
-		})
+	    })
 	}
 
 	$scope.random = function() {
-		$http.get('/api/code/get-random').success(function(data) {
+		$http.get('https://leekwars.com/api/leekscript/random').success(function(data) {
 			$scope.console = data.code;
 		})
 	}
