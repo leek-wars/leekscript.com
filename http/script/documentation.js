7cbc20d57c1743f7b9d96fc3d67689f442b87c06
Pilow <pierre.laupretre@gmail.com>
2017-07-10 22:01:10 +0200

    [doc] Fix doc page

--- http/script/documentation.js
+++ http/script/documentation.js
@@ -2,7 +2,7 @@ leekscript.controller('documentation_controller', function($scope, $route, $http
 
 	$scope.documentation = {}
 
-	$http.get('documentation.json').success(function(data) {
+	$http.get('/data/doc.json').success(function(data) {
 		$scope.documentation = data
 	})
 })
