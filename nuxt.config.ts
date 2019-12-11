f0526b33aeb33565678ced7fae621cd162ce8cee
Pilow <pierre.laupretre@gmail.com>
2019-12-11 19:07:19 +0100

    [config] Add theme color

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,8 @@ module.exports = {
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: 'Nuxt.js project' }
+			{ hid: 'description', name: 'description', content: 'Nuxt.js project' },
+			{ name: "theme-color", content: "#00ACC1" }
 		],
 		link: [
 			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
